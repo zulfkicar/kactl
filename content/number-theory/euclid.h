6f215118faa80e5f4cd970a4b71a20efe3a2f0e8
Hamza Hasan <hamzahasanellahie000@gmail.com>
2024-02-10 17:54:39 +0500

    gcd-euclid can be negative

--- content/number-theory/euclid.h
+++ content/number-theory/euclid.h
@@ -5,6 +5,7 @@
  * Description: Finds two integers $x$ and $y$, such that $ax+by=\gcd(a,b)$. If
  * you just need gcd, use the built in \texttt{\_\_gcd} instead.
  * If $a$ and $b$ are coprime, then $x$ is the inverse of $a \pmod{b}$.
+ * gcd can be negative
  */
 #pragma once
 
