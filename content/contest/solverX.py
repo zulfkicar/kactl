b20a157702ca7c6b5ba23a824278fefa111cda9d
zulfkicar <67015425+zulfkicar@users.noreply.github.com>
2024-04-11 04:21:08 +0500

    Add files via upload

--- content/contest/solverX.py
+++ content/contest/solverX.py
@@ -1,5 +1,10 @@
 from sys import setrecursionlimit
-import threading # kaizo
+import threading
+import functools
+#memoization
+@functools.lru_cache(maxsize=none) #add before a func you want to cache
+def f():
+    return 1
 setrecursionlimit(10**6+100)
 threading.stack_size(10**6)
 t=threading.Thread(target=solve)
--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -3,8 +3,9 @@ using namespace std;
 typedef long long int ll;
 typedef double dd;
 typedef long double ldd;
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define tst int T;cin>>T;for(int t=1;t<=T;t++)
-#define nl cout<<"\n";
+#define nl cout<<"\n"
 #define rep(i,l,r) for (int i=l;i<r;i++)
 #define per(i,r,l) for (int i=r;i>l;i--)
 #define pii pair<int,int>
@@ -22,20 +23,14 @@ const ll MOD2=998244353;
 ll mod=MOD1;
 ll po(ll x,ll y,ll _prime=mod) {if(y<0)return 0;y%=(_prime-1);ll res=1;while(y>0){if(y&1)res=(res*x)%_prime;x=(x*x)%_prime;y>>=1;}return (res%_prime);}
 ll gcd(ll a, ll b){if(a<b) swap(a,b);if(b==0) return a;return gcd(a%b,b);}
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rand(int lo, int hi){if(lo > hi) swap(lo,hi); return lo + rng() % (hi - lo + 1);} //returns in the range [lo, hi]
 
-void solve(){
-	
-}
-int main(){
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    #ifdef LOCAL
-        freopen("in.txt","r",stdin);
-        freopen("out.txt","w",stdout);
-    #endif
+void solve(){}
+int32_t main(){
+    fastio;
     tst
     {
         solve();nl;
     }
 }
-/*
-*/
